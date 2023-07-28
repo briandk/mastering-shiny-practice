e9fb00ff8ae95f27dcfdaa94b9553d6535e4e543
Brian A. Danielak <briandaniela.k+github@gmail.com>
2023-07-28 17:51:47 -0400

    Finish Ch. 1 Exercise 4
    Last commit message was wrong; should have said I completed exercise 3

--- app.R
+++ app.R
@@ -1,21 +1,26 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-  sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
-  "then x times y is",
-  textOutput("product")
+  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
+  "then, (x * y) is", textOutput("product"),
+  "and, (x * y) + 5 is", textOutput("product_plus5"),
+  "and (x * y) + 10 is", textOutput("product_plus10")
 )
 
 server <- function(input, output, session) {
-  x <- reactive({
-    get(input$x)
+  product <- reactive({
+    input$x * input$y
   })
   output$product <- renderText({
-    input$x * input$y
+    product()
+  })
+  output$product_plus5 <- renderText({
+    product() + 5
+  })
+  output$product_plus10 <- renderText({
+    product() + 10
   })
 }
 
 shinyApp(ui, server)
-# Run the application
-shinyApp(ui = ui, server = server)
