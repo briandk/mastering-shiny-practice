83cbd587a3d2331a8bb88950ac2891396d266e24
Brian A. Danielak <briandaniela.k+github@gmail.com>
2023-07-28 17:45:57 -0400

    Complete Ch. 1 Exercise 4

--- app.R
+++ app.R
@@ -1,31 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-  textInput("name", "What's your name?"),
-  textOutput("name")
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
+  "then x times y is",
+  textOutput("product")
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  username <- reactive({
-    get(input$name)
+server <- function(input, output, session) {
+  x <- reactive({
+    get(input$x)
   })
-
-  output$name <- renderText({
-    paste0("Hello ", input$name)
+  output$product <- renderText({
+    input$x * input$y
   })
-
 }
 
+shinyApp(ui, server)
 # Run the application
 shinyApp(ui = ui, server = server)
